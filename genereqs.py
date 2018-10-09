2edd57332d1b7ab0be7f78204424bd7d143449a0
Aaron Grossman <aarongrossman@gmail.com>
2018-10-08 23:41:58 -0400

    Cleaned and commented file, added readme.

--- genereqs.py
+++ genereqs.py
@@ -1,19 +1,59 @@
 #!/usr/bin/env python3
 def generate_requirements(globals):
+    """ 
+        Purpose
+        ----------
+        Generate requirements from a python file.
+
+        Parameters
+        ----------
+        globals : globals()
+        
+        To Use
+        ---------- 
+        Import this file into the file you wish to it in.
+        This can be done using "import genereqs" above where you
+        call the function. 
+
+        Once you have imported genereqs, call it by using
+        "genereqs.generate_requirements(globals())".        
+        This calls the module name, then the function name
+        and passes in a function containing the 
+        global namespace as a dictionary. The globals()
+        function is the only parameter and should be inserted
+        into the function call.     
+    """
+
     from pip._internal.operations import freeze
     import sys
     import os
-    l1 = list(set(sys.modules) & set(globals))
 
-    r = list(freeze.freeze())
-    d1 = dict()
-    for p in r:
-        j = p.split("=")[0].lower()
-        d1[j] = p
+    # Get a list of installed modules from sys and get the intersection
+    # with the result from the global variables defined in the file.
+    global_module_list = list(set(sys.modules) & set(globals))
 
-    g = list(d1[k] for k in (l1 & d1.keys()))
+    # Get list of modules from the python Freeze Command.
+    # This is done for versioning.
+    pip_freeze_list = list(freeze.freeze())
 
-    with open('requirements.txt', 'w') as f:
-        for rw in g:
-            f.write(f"{rw}\n")
-        f.truncate(f.tell() - len(os.linesep))
+    module_dictionary = dict()
+
+    for item in pip_freeze_list:
+        module_name = item.split("=")[0].lower()
+        module_dictionary[module_name] = item
+
+    modules_result = list()
+
+    # Get the list of each item that intersects between the 
+    # global module list  from above  
+    # and the keys of the dictionary from the Freeze list.
+
+    for key in (global_module_list & module_dictionary.keys()):
+        modules_result.append(module_dictionary[key])
+
+    with open('requirements.txt', 'w') as open_file:
+        for row in modules_result:
+            open_file.write(f"{row}\n")
+        # truncate the last \n and use os.linesep to determine 
+        # where this should be on linux or windows.
+        open_file.truncate(open_file.tell() - len(os.linesep))
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Genereqs
+
+This is a function for generating requirements for a python file as I wasn't satisfied with what I was able to find online.
